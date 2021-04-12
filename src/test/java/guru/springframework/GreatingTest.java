736625134e09fa0fcb9286aa0e8c87c3d7b6ecaf
Carlos Cavero <cavero.carlos@gmail.com>
2021-04-12 19:32:20 +0200

    Initialize Greeting object in BeforeEach

--- src/test/java/guru/springframework/GreatingTest.java
+++ src/test/java/guru/springframework/GreatingTest.java
@@ -1,20 +1,28 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
-    @Test void helloWorld() {
-        Greeting greeting = new Greeting();
+    Greeting greeting;
 
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each...");
+
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
         System.out.println(greeting.helloWorld());
     }
 
-    @Test void testHelloWorld() {
-        Greeting greeting = new Greeting();
-
+    @Test
+    void testHelloWorld() {
         System.out.println(greeting.helloWorld("Carlos"));
     }
 }
